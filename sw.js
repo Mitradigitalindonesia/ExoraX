01409b46b0b864d626b948ac73e9a64bfdaf6860
Mitradigitalindonesia <mitradigitalindonesia99@gmail.com>
2025-07-04 17:48:37 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,6 +4,7 @@ self.addEventListener("install", (event) => {
       return cache.addAll([
         "/",
         "/index.html",
+        "/manifest.json",
         "/icon-192.png",
         "/icon-512.png"
       ]);
